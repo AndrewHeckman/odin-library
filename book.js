f6f54f1ccbfdb71719232f8a16153a3fef188173
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-11-11 16:55:17 -0600

    Add new code to display books in table

--- book.js
+++ book.js
@@ -1,4 +1,5 @@
 const myLibrary = []
+const table = document.querySelector("#library-table-body");
 
 function Book(title, author, numPages, read) {
   this.title = title;
@@ -6,8 +7,32 @@ function Book(title, author, numPages, read) {
   this.numPages = numPages;
   this.read = read;
 
-   this.info = function() {
-    return `${this.title} by ${this.author}, ${numPages} pages, ${read ? "read" : "not read yet"}.`;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${numPages} page${numPages > 1 ? "s" :""}, ${read ? "read" : "not read yet"}.`;
+  }
+
+  this.createRow = function() {
+    const row = document.createElement("tr");
+    const title = document.createElement("td");
+    const author = document.createElement("td");
+    const pages = document.createElement("td");
+    const read = document.createElement("td");
+
+    title.textContent = this.title;
+    author.textContent = this.author;
+    pages.textContent = this.numPages.toString();
+    if (this.read) {
+      read.textContent = "read";
+    } else {
+      read.textContent = "unread";
+    }
+
+    row.appendChild(title);
+    row.appendChild(author);
+    row.appendChild(pages);
+    row.appendChild(read);
+
+    return row;
   }
 }
 
@@ -18,5 +43,11 @@ function addBookToLibrary(title, author, numPages, read) {
 function displayLibrary() {
   for (const book of myLibrary) {
     console.log(book.info());
+    table.appendChild(book.createRow());
   }
 }
+
+addBookToLibrary("title1", "author1", 1, true);
+addBookToLibrary("title2", "author2", 2, false);
+
+displayLibrary();
