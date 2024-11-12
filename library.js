9e68eb6859dbe46f3b5f9d7c1637fac1ffcf6102
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-11-11 18:08:30 -0600

    Add form submission and refactor book.js to library.js

--- library.js
+++ library.js
@@ -1,5 +1,10 @@
 const myLibrary = []
 const table = document.querySelector("#library-table-body");
+const dialog = document.querySelector("#new-book-dialog");
+const form = document.querySelector("form");
+const submitButton = document.querySelector("#form-submit");
+const closeButton = document.querySelector("#form-close");
+const dialogButton = document.querySelector("#new-book-button");
 
 function Book(title, author, numPages, read) {
   this.title = title;
@@ -36,18 +41,40 @@ function Book(title, author, numPages, read) {
   }
 }
 
-function addBookToLibrary(title, author, numPages, read) {
-  myLibrary.push(new Book(title, author, numPages, read));
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  table.appendChild(book.createRow());
 }
 
 function displayLibrary() {
   for (const book of myLibrary) {
     console.log(book.info());
-    table.appendChild(book.createRow());
   }
 }
 
-addBookToLibrary("title1", "author1", 1, true);
-addBookToLibrary("title2", "author2", 2, false);
+function handleSubmit() {
+  const title = document.querySelector("#form-title").value;
+  const author = document.querySelector("#form-author").value;
+  const pages = parseInt(document.querySelector("#form-pages").value);
+  const read = document.querySelector("#form-read").checked;
+
+  addBookToLibrary(new Book(title, author, pages, read));
+}
+
+submitButton.addEventListener("click", function (event){
+  event.preventDefault();
+  handleSubmit();
+});
+
+closeButton.addEventListener("click", function () {
+  dialog.close();
+})
+
+dialogButton.addEventListener("click", function () {
+  dialog.showModal();
+})
+
+addBookToLibrary(new Book("title1", "author1", 1, true));
+addBookToLibrary(new Book("title2", "author2", 2, false));
 
 displayLibrary();
